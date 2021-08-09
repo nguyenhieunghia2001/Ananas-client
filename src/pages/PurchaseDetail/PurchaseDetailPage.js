bdd2b9958777eac493b8f8fa73f7b72f71e3724b
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-08-09 15:28:20 +0700

    UI purchase detail [art 1 :3

--- src/pages/PurchaseDetail/PurchaseDetailPage.js
+++ src/pages/PurchaseDetail/PurchaseDetailPage.js
@@ -9,7 +9,12 @@ const PurchaseDetail = () => {
         <header>
           <h2>THÔNG TIN ĐƠN HÀNG</h2>
         </header>
-        <div className="progress-status"></div>
+        <div className="progress-status">
+          <div className="progress-1">ĐẶT HÀNG THÀNH CÔNG</div>
+          <div className="progress-2">ĐẶT HÀNG THÀNH CÔNG</div>
+          <div className="progress-3">ĐẶT HÀNG THÀNH CÔNG</div>
+          <div className="progress-4">ĐẶT HÀNG THÀNH CÔNG</div>
+        </div>
         <div className="info">
           <Row>
             <Col lg={6}>
@@ -29,7 +34,7 @@ const PurchaseDetail = () => {
               </div>
             </Col>
             <Col lg={6}>
-            <div className="wrapper">
+              <div className="wrapper">
                 <div className="top">
                   <h4>THÔNG TIN GIAO NHẬN</h4>
                 </div>
@@ -44,8 +49,75 @@ const PurchaseDetail = () => {
                 </div>
               </div>
             </Col>
-            <Col lg={6}></Col>
-            <Col lg={6}></Col>
+            <Col lg={6}>
+              <div className="wrapper">
+                <div className="top">
+                  <h4>DANH SÁCH SẢN PHẨM</h4>
+                </div>
+                <div className="content">
+                  <Row>
+                    <Col lg={4}>
+                      <div className="product-img">
+                        <img
+                          src="https://ananas.vn/wp-content/uploads/Pro_AV00011_1.jpg"
+                          alt="product"
+                        />
+                      </div>
+                    </Col>
+                    <Col lg={8}>
+                      <div className="product-info">
+                        <h6>Basas Hook N'Loop NE - Low Top - White</h6>
+                        <div className="cont">
+                          <span className="title">Giá: </span>
+                          <span>520.000 VNĐ</span>
+                        </div>
+                        <div className="cont">
+                          <span className="title">Size: </span>
+                          <span>35</span>
+                        </div>
+                        <div className="cont">
+                          <span className="title">Số lượng: </span>
+                          <span>1</span>
+                        </div>
+                        <h6 className="total">520.000 VNĐ</h6>
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+              </div>
+            </Col>
+            <Col lg={6}>
+              <div className="wrapper">
+                <div className="top">
+                  <h4>THANH TOÁN</h4>
+                </div>
+                <div className="content">
+                  <div className="group">
+                    <span>Trị giá đơn hàng:</span>
+                    <p>520.000 VNĐ</p>
+                  </div>
+                  <div className="group">
+                    <span>Giảm giá:</span>
+                    <p>0 VND</p>
+                  </div>
+                  <div className="group">
+                    <span>Phí giao hàng:</span>
+                    <p>20.000 VND</p>
+                  </div>
+                  <div className="group">
+                    <span>Phí thanh toán:</span>
+                    <p>0 VND</p>
+                  </div>
+                </div>
+                <div className="divider-img"></div>
+                <div className="bottom">
+                  <div className="group">
+                    <span className="total">Tổng thanh toán:</span>
+                    <p>540.000 VNĐ</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
           </Row>
         </div>
       </Container>
