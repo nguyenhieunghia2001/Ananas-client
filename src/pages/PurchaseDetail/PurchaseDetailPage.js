2508eff052009d43a832bd5813679867a12a73cd
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-08-09 20:41:47 +0700

    purchase detail, complete!!

--- src/pages/PurchaseDetail/PurchaseDetailPage.js
+++ src/pages/PurchaseDetail/PurchaseDetailPage.js
@@ -10,11 +10,61 @@ const PurchaseDetail = () => {
           <h2>THÔNG TIN ĐƠN HÀNG</h2>
         </header>
         <div className="progress-status">
-          <div className="progress-1">ĐẶT HÀNG THÀNH CÔNG</div>
-          <div className="progress-2">ĐẶT HÀNG THÀNH CÔNG</div>
-          <div className="progress-3">ĐẶT HÀNG THÀNH CÔNG</div>
-          <div className="progress-4">ĐẶT HÀNG THÀNH CÔNG</div>
+          <div className="top">
+            <h4>
+              TRẠNG THÁI ĐƠN HÀNG <span className="id">W0221924</span>
+            </h4>
+            <span>Thanh toán COD - Tốc độ tiêu chuẩn</span>
+          </div>
+          <Row style={{ margin: "0" }}>
+            <Col lg={3} md={6} sm={6} xs={12}>
+              <div className="progress-1 active">ĐẶT HÀNG THÀNH CÔNG</div>
+              <div className="cont active">
+                <span className="time">Vào lúc 10:04 - 02/08/2021</span>
+                <span>
+                  Thời gian xử lý đơn hàng có thể từ 1-2 ngày làm việc. Vui lòng
+                  gọi đến hotline 0963 429 749 (trong giờ hành chính) nếu bạn
+                  muốn thay đổi thông tin đơn hàng trước khi đơn hàng của bạn
+                  được CHUYỂN QUA GIAO NHẬN.
+                </span>
+              </div>
+            </Col>
+            <Col lg={3} md={6} sm={6} xs={12}>
+              <div className="progress-2">ĐƠN HÀNG ĐÃ HỦY</div>
+              <div className="cont">
+                <span className="time">Vào lúc 10:06 - 02/08/2021</span>
+                <span>
+                  Đơn hàng của bạn đã bị huỷ bởi một trong những lí do sau: -
+                  Bạn chủ động huỷ nó. - Thẻ thanh toán của bạn không được chấp
+                  nhận. - Chúng tôi không liên lạc được hoặc số điện thoại trong
+                  thông tin của bạn bị nhầm lẫn. - Sản phẩm bạn đặt không đủ
+                  điều kiện để chúng tôi giao hàng ra khỏi kho (hết hàng hoặc
+                  sản phẩm bị lỗi).
+                </span>
+              </div>
+            </Col>
+            <Col lg={3} md={6} sm={6} xs={12}>
+              <div className="progress-3">ĐANG GIAO HÀNG</div>
+              <div className="cont ">
+                <span>
+                  Thời gian giao hàng tuỳ thuộc vào địa điểm và phương thức giao
+                  hàng bạn đã chọn. Hãy tin rắng chúng tôi luôn cố gắng để hàng
+                  đến tay bạn sớm nhất!
+                </span>
+              </div>
+            </Col>
+            <Col lg={3} md={6} sm={6} xs={12}>
+              <div className="progress-4">GIAO HÀNG THÀNH CÔNG</div>
+              <div className="cont ">
+                <span>
+                  Đơn hàng đã được giao thành công ! Chúc bạn có một trải nghiệm
+                  thú vị ^^
+                </span>
+              </div>
+            </Col>
+          </Row>
         </div>
+        <div className="divider-img"></div>
         <div className="info">
           <Row>
             <Col lg={6}>
@@ -56,7 +106,7 @@ const PurchaseDetail = () => {
                 </div>
                 <div className="content">
                   <Row>
-                    <Col lg={4}>
+                    <Col lg={4} md={4} sm={4} xs={4}>
                       <div className="product-img">
                         <img
                           src="https://ananas.vn/wp-content/uploads/Pro_AV00011_1.jpg"
@@ -64,7 +114,7 @@ const PurchaseDetail = () => {
                         />
                       </div>
                     </Col>
-                    <Col lg={8}>
+                    <Col lg={8} md={8} sm={8} xs={8}>
                       <div className="product-info">
                         <h6>Basas Hook N'Loop NE - Low Top - White</h6>
                         <div className="cont">
