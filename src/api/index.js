430769ee207940ee51249539b49ebb923347062f
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-08-26 08:46:25 +0700

    env api url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL:  process.env.API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
 
