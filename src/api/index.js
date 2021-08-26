b49a60c7213adf595ca683fbeb1cfbfeaece3bee
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-08-26 08:58:19 +0700

    try env not working

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://api-ananas.herokuapp.com/',
   withCredentials: true,
 });
 
